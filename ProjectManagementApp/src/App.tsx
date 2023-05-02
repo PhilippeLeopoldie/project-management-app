5c258765428e90f2147ed1d002c31e5a7527a282
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-05-02 18:55:56 +0200

    project setting

---
+++ ProjectManagementApp/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProjectsPage from "./projects/ProjectsPage";
+
+function App() {
+  return (
+    <div className="Container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
