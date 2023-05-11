c1c3fb82d164c9516063b93147898bc8059795cf
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-05-11 17:05:13 +0200

    create type ProjectInitializer to avoid any

--- ProjectManagementApp/src/projects/Project.ts
+++ ProjectManagementApp/src/projects/Project.ts
@@ -1,25 +1,35 @@
+type ProjectInitializer ={
+  id?:number;
+  name?:string;
+  description?:string;
+  imageUrl?: string;
+  contractTypeId:number;
+  contractSignedOn?:string;
+  budget?:number;
+  isActive?: boolean;
+}
 export class Project{
   id : number | undefined;
   name = '';
   description ='';
   imageUrl ='';
   contractTypeId: number | undefined;
-  contractSigneOn: Date = new Date();
+  contractSignedOn: Date = new Date();
   budget = 0;
   isActive = false;
   get isNew():boolean{
     return this.id === undefined
   }
 
-  constructor(initializer?: any){
+  constructor(initializer?: ProjectInitializer){
     if (!initializer) return;
     if (initializer.id) this.id = initializer.id;
     if (initializer.name) this.name = initializer.name;
     if (initializer.description) this.description = initializer.description;
     if (initializer.imageUrl) this.imageUrl = initializer.imageUrl;
     if (initializer.contractTypeId) this.contractTypeId = initializer.contractTypeId;
-    if (initializer.contractSigneOn)
-    this.contractSigneOn = new Date(initializer.contractSigneOn);
+    if (initializer.contractSignedOn)
+    this.contractSignedOn = new Date(initializer.contractSignedOn);
     if (initializer.budget) this.budget = initializer.budget;
     if (initializer.isActive) this.isActive = initializer.isActive;
   }
