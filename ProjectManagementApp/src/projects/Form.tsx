179ee870f4ebe96ae2541752db8c167fbe6c5deb
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-05-22 19:46:49 +0200

    testing validation select & textarea

--- ProjectManagementApp/src/projects/Form.tsx
+++ ProjectManagementApp/src/projects/Form.tsx
@@ -1,60 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Form() {
- const [departement, setDepartement] = useState<string>('');
- const [message, setMessage]= useState<string>('');
- const [agreedToTerms, setAgreedToTerms] = useState<boolean>(false);
+  const [departement, setDepartement] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [agreedToTerms, setAgreedToTerms] = useState<boolean>(false);
 
- const [departementError, setDepartementError] = useState<string|null>(null);
- const [messageError, setMessageError] = useState<string | null>(null);
- const [agreedToTermsError, setAgreedToTermsError] = useState<string | null>(null);
+  const [departementError, setDepartementError] = useState<string | null>(null);
+  const [messageError, setMessageError] = useState<string | null>(null);
+  const [agreedToTermsError, setAgreedToTermsError] = useState<string | null>(
+    null
+  );
 
- const [departementTouched, setDepartementTouched] = useState<boolean>(false);
- const [messageTouched, setMessageTouched] = useState<boolean>(false);
- const [agreedTotermsTouched, setAgreedToTermsTouched] = useState<boolean>(false);
+  const [departementTouched, setDepartementTouched] = useState<boolean>(false);
+  const [messageTouched, setMessageTouched] = useState<boolean>(false);
+  const [agreedTotermsTouched, setAgreedToTermsTouched] =
+    useState<boolean>(false);
 
-  const handleSubmit = (event:React.FormEvent<HTMLFormElement> ) =>{
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  
 
-  const isValid = !departementError && !messageError && !agreedToTermsError;
+    const isValid = !departementError && !messageError && !agreedToTermsError;
 
-  if(!isValid) return;
-  console.log('submitting', {departement,message,agreedToTerms})
-  }
+    if (!isValid) return;
+    console.log("submitting", { departement, message, agreedToTerms });
+  };
 
-  const validate=() => {
+  const validate = () => {
     setDepartementError(null);
     setMessageError(null);
     setAgreedToTermsError(null);
 
-    if(departement==='') setDepartementError('Departement is Required');
-    if(message ==='') setMessageError('Message is required');
-    if(agreedToTerms === false) setAgreedToTermsError('You must agree to the terms and conditions');
-  }
+    if (departement === "") setDepartementError("Departement is Required");
+    if (message === "") setMessageError("Message is required");
+    if (agreedToTerms === false)
+      setAgreedToTermsError("You must agree to the terms and conditions");
+  };
 
-  useEffect(()=> {
-    validate()
-  }, [message,agreedToTerms,departement])
+  useEffect(() => {
+    validate();
+  }, [message, agreedToTerms, departement]);
 
-  const stateToString= () =>{
+  const stateToString = () => {
     return JSON.stringify({
-      values:{message,departement,agreedToTerms},
-      errors: {messageError, departementError, agreedToTermsError},
-      touched: {messageTouched,setDepartementTouched, agreedTotermsTouched}
-    })
+      values: { message, departement, agreedToTerms },
+      errors: { messageError, departementError, agreedToTermsError },
+      touched: { messageTouched, departementTouched, agreedTotermsTouched },
+    });
   };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <select
+        name="departement"
+        value={departement}
+        onChange={(event) => setDepartement(event.target.value)}
+        onBlur={() => {
+          setDepartementTouched(true);
+        }}
+      >
+        <option value="">Select...</option>
+        <option value="hr">Human Ressources</option>
+        <option value="support">Support</option>
+      </select>
+      <br />
+      {departementError && departementTouched && (
+        <p className="alert">{departementError}</p>
+      )}
+      <br />
+      <textarea
+        name="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onBlur={(e) => setMessageTouched(true)}
+        
+      />
       
 
-
-     
-
-
-      
+      <></>
     </form>
-  )
+  );
 }
 
-export default Form
+export default Form;
